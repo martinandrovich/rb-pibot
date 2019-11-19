7211de1eecf2096dbb2c92d0e86133115f196e3c
Martin Androvich <martinandrovich@gmail.com>
2019-11-19 17:14:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-# https://realpython.com/python-main-function/
+from server import *
+from core import *    
+
 def main():
+    
+    # hello
     print("Hello World!")
+    
+    # init server + commands table
+    server.init()
+    server.commands = { 
+        'getdist':   core.get_dist(),
+        'getmotors': core.get_motors()
+    }
+    
+    # init controller
+    core.init()
+    
+    # main loop
+    while True:
+        server.operate()
+        core.operate()
 
 if __name__ == "__main__":
     main()
